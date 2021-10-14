222925f06962d69ada104c64a2766683c81d685c
yxkumad <46276046+yxkumad@users.noreply.github.com>
2021-10-15 01:36:39 +0800

    Update monkey.sh

--- monkey.sh
+++ monkey.sh
@@ -31,7 +31,7 @@ function Initialize {
 }
 
 function Test {
-    if [ $MODE -eq 4]; then
+    if [[ $MODE -eq 4 ]]; then
         echo "Detecting Netflix Unlock..."        
         Netflix=$(curl --interface $WGName --user-agent "${UA_Browser}" -4 -fsL --write-out %{http_code} --output /dev/null --max-time 30 "https://www.netflix.com/title/70143836" 2>&1)
         echo "Detecting Google Location..."
@@ -58,18 +58,28 @@ function Analyse {
         NetflixResult="Error"
     fi
     if [[ "$Google" == "" ]]; then
-        Google="CN"
+        if [[ $MODE -eq 4 ]]; then
+            tmpresult=$(curl --interface $WGName -4 -sS -H "Accept-Language: en" "https://www.youtube.com/premium" 2>&1 )
+        else
+            tmpresult=$(curl -4 -sS -H "Accept-Language: en" "https://www.youtube.com/premium" 2>&1 )
+        fi
+        isCN=$(echo $tmpresult | grep 'www.google.cn')
+            if [ -n "$isCN" ]; then
+                Google="CN"
+            else	
+                Google="US"
+            fi
     fi
     echo "Netflix Result: $NetflixResult"
     echo "Google Result: $Google"
     if [[ "$Netflix" == "404" ]] || [[ "$Netflix" == "403" ]] || [[ "$Netflix" == "000" ]] || [[ "$Google" == "CN" ]]; then
         ChangeIP
     else
-        if [[ $COUNT == 0 ]]; then
+        if [[ $COUNT -eq 0 ]]; then
             echo "No error found. Exiting..."
             rm -rf /usr/local/bin/.netflix_session
         else
-            if [ $DNSmasq -eq 1 ]; then
+            if [[ $DNSmasq -eq 1 ]]; then
                 ChangeDNS
             fi
             SendMsg
@@ -82,7 +92,7 @@ function Analyse {
 function ChangeIP {
     echo "Trying to change IP..."
     let COUNT++
-    if [ $MODE -eq 1 ]; then
+    if [[ $MODE -eq 1 ]]; then
         dhclient -r -v >/dev/null 2>&1
         rm -rf /var/lib/dhcp/dhclient*
         ps aux | grep dhclient | grep -v grep | awk -F ' ' '{print $2}' | xargs kill -9 2>/dev/null
@@ -90,21 +100,21 @@ function ChangeIP {
         dhclient -v >/dev/null 2>&1
         sleep 5s
         Test
-    elif [ $MODE -eq 2 ]; then
+    elif [[ $MODE -eq 2 ]]; then
         curl $API > /dev/null 2>&1
         sleep 10s
         Test
-    elif [ $MODE -eq 3 ]; then
+    elif [[ $MODE -eq 3 ]]; then
         Cookie=$(curl -ifsSL $'$SYMAPI' 2>&1 | grep -o PHPSESSID=[0-9a-z]*)
         curl $'$SYMAPI' -H 'Cookie: '$Cookie > /dev/null 2>&1
         sleep 10s
         Test
-    elif [ $MODE -eq 4 ]; then
+    elif [[ $MODE -eq 4 ]]; then
         wg-quick down $WGName >/dev/null 2>&1
         sleep 5s
         wg-quick up $WGName >/dev/null 2>&1
         Test
-    elif [ $MODE -eq 5 ]; then
+    elif [[ $MODE -eq 5 ]]; then
         SendMsg
         curl $API > /dev/null 2>&1
     fi
